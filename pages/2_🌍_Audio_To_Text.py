a7762a98d451d9374a143b7754e8b9f7b6527eb2
Thomas Kim <kimsh92@hotmail.com>
2025-04-17 10:58:18 +0900

    change selectbox w/ env_config

--- pages/2_🌍_Audio_To_Text.py
+++ pages/2_🌍_Audio_To_Text.py
@@ -3,7 +3,7 @@ import streamlit as st
 import streamlit.components.v1 as components
 
 import env_config as env
-from env_config import init_logger, init_config
+from env_config import init_logger, init_config, llm_selector, translation_selector, llm_server_selector
 from appai_client import AIClient
 from audio_recorder_streamlit import audio_recorder
 
@@ -22,37 +22,6 @@ if 'logger' not in st.session_state:
     
 #######################################################################
 #
-def translation_selector():
-    with st.sidebar:
-        return st.selectbox("Select Translation Language",
-                            sorted(list(supported_languages.keys())[1:]),
-                            index=8,
-                            label_visibility="visible")
-def llm_selector():
-    llm_models = ["gpt-4o", "meta-llama-3.1-8b-instruct"] 
-    with st.sidebar:
-        if 'llm_model_option' not in st.session_state:
-            st.session_state.llm_model_option = 0
-        
-        llm_model = st.selectbox("Model", llm_models, index=st.session_state.llm_model_option)
-        st.session_state.logger.info(f"Selected LLM Model : {llm_model}")
-        st.session_state.llm_model_option = llm_models.index(llm_model)
-        
-        return llm_model
-
-def llm_server_selector():
-    llm_servers=["LocalAI", "Ollama","OpenAI"]
-    with st.sidebar:
-        with st.sidebar:
-            if 'llm_server_option' not in st.session_state:
-                st.session_state.llm_server_option = 0
-                
-        llm_server = st.selectbox("LLM Server", llm_servers, index=st.session_state.llm_server_option)
-        st.session_state.logger.info(f"Selected LLM Server : {llm_server}")
-        st.session_state.llm_model_option = llm_servers.index(llm_server)
-        
-        return llm_server
-
 with st.sidebar:
     st.title("🎙️ Audio To Text")  
         
@@ -61,6 +30,8 @@ with st.sidebar:
     if translate_option:
         target_language = translation_selector()
         st.session_state.logger.info(f"Select Lanauge : {target_language}")
+
+    st.session_state.llm_model = llm_selector(disabled=True)
 
     # Generate Audio Option
     #genaudio_option = st.checkbox("Enable Generate Audio")          
@@ -78,7 +49,7 @@ if 'ai_client' not in st.session_state:
     # Get an OpenAI/LocalAI/Ollama Client
     ai_client = AIClient(api_key=env.api_key, 
                          base_url=env.base_url,
-                         model=llm_selector())
+                         model=llm_selector(disabled=True))
 else:
     ai_client = st.session_state.ai_client
 
